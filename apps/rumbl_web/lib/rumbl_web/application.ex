9f8535a47a5dbba637266421d5a0b5da72b1bd65
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-02-19 15:35:38 +0100

    Umbrella

---
+++ apps/rumbl_web/lib/rumbl_web/application.ex
@@ -0,0 +1,22 @@
+defmodule RumblWeb.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      RumblWeb.Endpoint,
+      RumblWeb.Presence
+    ]
+
+    opts = [strategy: :one_for_one, name: RumblWeb.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    RumblWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
